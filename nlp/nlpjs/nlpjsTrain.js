b0869551f00807d6bc69f3f755c5cd90fd492902
Stuart Hsu <yopahsu@gmail.com>
2020-02-05 09:02:42 +0800

    Set nlpjs model for title politician detection

--- nlp/nlpjs/nlpjsTrain.js
+++ nlp/nlpjs/nlpjsTrain.js
@@ -2,7 +2,7 @@ const { NlpManager } = require('node-nlp');
 
 const manager = new NlpManager({ languages: ['zh'] });
 
-const politician = ['賴清德','韓國瑜','蘇貞昌','王世堅','郝龍斌'];
+const politician = ['賴清德','韓國瑜','蘇貞昌','王世堅','郝龍斌','柯文哲'];
 const country = ['美國','中國','中國大陸','大陸','中共'];
 
 manager.addNamedEntityText(
@@ -37,10 +37,12 @@ manager.addDocument('zh', '%politician%：', 'politician.say');
 manager.addDocument('zh', '%politician%%say%', 'politician.say');
 manager.addDocument('zh', '%country%：', 'country.say');
 manager.addDocument('zh', '%country%%say%', 'country.say');
+manager.addDocument('zh', '%say%', 'someone.say');
 manager.addDocument('zh', '%medical%：', 'issue.medical');
 
 manager.addAnswer('zh', 'politician.say', '政治人物表示');
 manager.addAnswer('zh', 'country.say', '國家表示');
+manager.addAnswer('zh', 'someone.say', '某人表示');
 manager.addAnswer('zh', 'issue.medical', '醫療議題');
 
 
@@ -49,7 +51,7 @@ module.exports = {
     return new Promise(async function(resolve, reject) {
       await manager.train();
       manager.save('./nlp/nlpjs/trained_model/train.nlp');
-      const content = '影／喊蓋收容所被稱先知！韓國瑜：我們是先辦，高雄希望做防疫模範生'.replace('：', '表示');
+      const content = '爆料隔離地點恐挨罰 柯文哲稱「會檢討」：要被抓去關了嗎？'.replace('：', '表示');
       const response = await manager.process('zh', content);
       resolve (JSON.stringify(response));
     });
