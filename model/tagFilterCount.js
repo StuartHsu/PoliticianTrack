500c6dec2eac1403ccf8e9e092c1d909aa3918da
Stuart Hsu <yopahsu@gmail.com>
2020-02-28 00:08:28 +0800

    Modify index tag present logic, to show tags witch must have news

--- model/tagFilterCount.js
+++ model/tagFilterCount.js
@@ -4,7 +4,15 @@ const mysql = require("../util/mysqlcon.js");
 module.exports = {
   get: function() {
     return new Promise(async function(resolve, reject) {
-      let sql = 'select * from filtercount order by count desc;';
+      // let sql = 'select * from filtercount order by count desc;';
+      let sql = `
+      SELECT a.id AS tag_id, a.name, a.type, count(a.name) AS count
+      FROM filtercount AS a
+      LEFT JOIN newstag AS b ON (a.id = b.tag_id)
+      LEFT JOIN news AS c ON (b.news_id = c.id)
+      WHERE c.intent = "politician_say"
+      GROUP BY a.id ORDER BY count DESC;
+      `;
       mysql.con.query(sql, function(error, results, fields) {
         if(error) {
           reject(error);
