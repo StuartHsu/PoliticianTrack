668426ef0c341ab8a41ac1201844e7064f9e4787
Stuart Hsu <yopahsu@gmail.com>
2020-02-29 20:16:14 +0800

    Fix news gets error @compare page due to add parent/child judge cause sql syntax error

--- model/news.js
+++ model/news.js
@@ -425,16 +425,19 @@ function getNewsId(tagId) {
     let sql;
     if(tagId.length < 2) { // 單人
       sql = `
-      SELECT a.*, b.id, b.name, b.parent_id FROM newstag AS a
-      LEFT JOIN filtercount AS b ON (a.tag_id = b.parent_id)
-      WHERE b.parent_id IN (?);
+        SELECT a.*, b.id, b.name, b.parent_id FROM newstag AS a
+        LEFT JOIN filtercount AS b ON (a.tag_id = b.parent_id)
+        WHERE b.parent_id IN (?);
       `;
     } else { // 人與議題
       sql = `
-      SELECT a.news_id FROM newstag AS a
-      LEFT JOIN filtercount AS b ON (a.tag_id = b.parent_id)
-      WHERE b.parent_id IN (?)
-      GROUP BY news_id HAVING count(*) > 1;
+      SELECT c.id AS news_id
+      FROM filtercount AS a
+      LEFT JOIN newstag AS b ON (a.id = b.tag_id)
+      LEFT JOIN news AS c ON (b.news_id = c.id)
+      LEFT JOIN filtercount AS d ON (a.parent_id = d.id)
+      WHERE c.intent = "politician_say" AND d.parent_id IN (?)
+      GROUP BY c.id HAVING count(*) > 1
       `;
     }
     let data = [];
